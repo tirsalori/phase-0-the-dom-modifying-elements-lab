017c8966dbf4b0acf0137c527a276c79fe3640d6
Tirsa St. Fort <lifelovelori@gmail.com>
2022-12-28 19:03:46 -0800

    commit tree 30efa77741c018b8cf30a79a904f6bf73f9340f8

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementById("main")
+element.remove("main")
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.textContent = "Tirsa is the champion"
